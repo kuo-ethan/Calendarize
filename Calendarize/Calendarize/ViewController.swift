0180b03a4eaa81adea9e759a0829972251b6b69f
Ethan Kuo <ek0335@berkeley.edu>
2022-12-14 15:23:45 -0800

    starting fresh

---
+++ Calendarize/Calendarize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calendarize
+//
+//  Created by Ethan Kuo on 12/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
